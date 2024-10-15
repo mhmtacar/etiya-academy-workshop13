75525bb48f646cdb24054737cd6482f21bf3c54b
Mehmet Acar <mehmet-acar-gs@hotmail.com>
2024-10-15 13:53:39 +0300

    workshop 12 completed

--- src/app/shared/components/todo-list/todo-list.component.ts
+++ src/app/shared/components/todo-list/todo-list.component.ts
@@ -1,12 +1,32 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-todo-list',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './todo-list.component.html',
   styleUrl: './todo-list.component.scss'
 })
 export class TodoListComponent {
+  todoList: string[] = [];
+  todoInput: string = '';
+
+  isTodoExists(todo : string): boolean{
+    return this.todoList.includes(todo)
+  }
+
+  deleteTodo(index : number): void{
+    this.todoList.splice(index,1);
+  }
+
+  addTodo(): void {
+    if (this.isTodoExists(this.todoInput)) {
+      alert('This todo is already in the list!');
+    } else {
+      this.todoList.push(this.todoInput);
+    }
+    this.todoInput = "";
+  }
 
 }
